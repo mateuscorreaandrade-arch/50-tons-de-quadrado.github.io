e65a066a61e6630b75a8ea4f19f41b57d80f40d1
mateuscorreaandrade-arch <mateus.correa.andrade@escola.pr.gov.br>
2026-09-02 07:57:50 -0300

    Add dark mode toggle and text change functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,49 @@
+<script src="script.js"></script>
+</body>
 
+</html>
+
+script.js
+// =============================
+// MODO ESCURO
+// =============================
+
+const botaoTema =
+document.getElementById("botaoTema");
+
+function alternarTema() {
+
+document.body.classList.toggle("escuro");
+
+if (document.body.classList.contains("escuro")) {
+
+botaoTema.textContent = "☀ Modo claro";
+
+} else {
+
+botaoTema.textContent = "🌙 Modo escuro";
+
+}
+}
+
+// =============================
+// ALTERAR TEXTO
+// =============================
+
+const botaoMensagem =
+document.getElementById("botaoMensagem");
+
+const mensagem =
+document.getElementById("mensagem");
+
+function alterarTexto() {
+
+mensagem.textContent =
+"O texto foi alterado pelo JavaScript!";
+
+}
+
+botaoMensagem.addEventListener("click", alterarTexto);
+
+botaoTema.addEventListener("click", alternarTema
+                          
